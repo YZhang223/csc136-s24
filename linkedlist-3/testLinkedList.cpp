bf2b79c99b57a8135c7f84ffd303f54c09861794
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2024-04-15 08:41:28 -0400

    Update testLinkedList.cpp

--- linkedlist-3/testLinkedList.cpp
+++ linkedlist-3/testLinkedList.cpp
@@ -16,6 +16,35 @@ int main()
 
   myList.print();
   
+   //testing search()
   cout << "Search for 9: " << myList.search(9) << endl;
   cout << "Search for 10: " << myList.search(10) << endl;
+
+  /*
+  //testing deleteNode()
+  myList.deleteNode(7);
+  myList.print();
+  
+  
+  //testing copy constructor
+  linkedList yourList(myList);
+  yourList.insertAtEnd(11);
+
+  myList.print();
+  yourList.print();
+
+
+  //testing destroylist() function
+  yourList.destroyList();
+  yourList.print();
+
+
+  //test assignment operator
+  linkedList yourList2;
+  yourList2 = myList;
+  yourList2.insertAtEnd(15);
+  yourList2.insertAtEnd(17);
+  myList.print();
+  yourList2.print();
+  */
 }
